f43fca190bc2d50a0b4234a3cec8a95f9ff5a19e
Syava <ButuzGOL@gmail.com>
2015-06-10 10:22:23 +0300

    Update factory.js

--- factory.js
+++ factory.js
@@ -51,3 +51,36 @@ console.log(car instanceof Car);
  
 // Outputs: Car object of color "yellow", doors: 6 in a "brand new" state
 console.log(car);
+
+var movingTruck = carFactory.createVehicle( {
+                      vehicleType: "truck",
+                      state: "like new",
+                      color: "red",
+                      wheelSize: "small" } );
+ 
+// Test to confirm our truck was created with the vehicleClass/prototype Truck
+ 
+// Outputs: true
+console.log(movingTruck instanceof Truck);
+ 
+// Outputs: Truck object of color "red", a "like new" state
+// and a "small" wheelSize
+console.log(movingTruck);
+
+function TruckFactory () {}
+TruckFactory.prototype = new VehicleFactory();
+TruckFactory.prototype.vehicleClass = Truck;
+ 
+var truckFactory = new TruckFactory();
+var myBigTruck = truckFactory.createVehicle( {
+                    state: "omg..so bad.",
+                    color: "pink",
+                    wheelSize: "so big" } );
+ 
+// Confirms that myBigTruck was created with the prototype Truck
+// Outputs: true
+console.log(myBigTruck instanceof Truck);
+ 
+// Outputs: Truck object with the color "pink", wheelSize "so big"
+// and state "omg. so bad"
+console.log(myBigTruck);
